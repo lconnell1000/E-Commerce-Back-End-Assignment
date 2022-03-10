80b37f243a7366fbda27ea1e5527de7ed9489bdd
lconnell1000 <lconnell1000@gmail.com>
2022-03-10 16:27:46 +1030

    product model done

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,41 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    // define id column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    //define prouct name colum
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    //define price column
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    //define stock column
+    stock: {
+      type: DataTypes.INTEGER,
+      validate: {
+        isNumeric: true
+      }
+    },
+    //define category id column
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
